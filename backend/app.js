7fe555e9ba1248072fd27127fea5e027d41c06b0
Basix <jsrtyu1120@gmail.com>
2017-04-15 16:21:36 +0900

    Hello World setup for now.

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,12 @@
 const express = require('express')
+const app = express()
+
+const PORT = require('../config').port
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`)
+})
