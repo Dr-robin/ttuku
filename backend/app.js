9d8d4a23102db76e5d14bd91c58c694031e4868c
Basix <jsrtyu1120@gmail.com>
2017-04-15 16:34:36 +0900

    socket.io connection

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const io = require('socket.io')(http)
 
 const PORT = require('../config').port
 
+io.on('connection', _ => console.log('a user connected') )
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
